429bd0777d15b2d689e9d9b0e5aebd8852f88aea
VadimChorrny <chorrny228@gmail.com>
2021-04-09 20:59:55 +0300

    Добавьте файлы проекта.

---
+++ Kursova/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kursova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string text = "Ось будинок, який побудував Джек." +
+                "А це пшениця, Яка в темній комірці зберігається У будинку," +
+                "Який побудував Джек. А це весела птиця-синиця," +
+                "Яка часто краде пшеницю, Яка в темній комірці зберігається У будинку," +
+                "Який побудував Джек.";
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            string[] newText = text.Split(' ');
+            foreach (var item in newText)
+            {
+                string tmp = item;
+                item.Trim('.', ',');
+                if(words.ContainsKey(item))
+                {
+                    int tmpValue = 1;
+                    words.TryGetValue(item, out tmpValue);
+                    tmpValue++;
+                    words[item] = tmpValue;
+                }
+                else
+                {
+                    words.Add(item, 1);
+                }
+            }
+
+            int id = 0;
+            Console.WriteLine($"{"Word",20}{"Count",20}");
+            foreach (var item in words)
+            {
+                ++id;
+                Console.WriteLine($"{id,0 }. {item.Key,20} {item.Value,20}");
+
+            }
+            Console.WriteLine($"All word {words.Count}, unique { (words.Where(x => x.Value == 1).ToArray().Length)}");
+
+        }
+    }
+}
---
+++ Second_Task/Dict.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Second_Task
+{
+    public enum TypeDictionary { ANGLO_UKR = 1,UKR_ANGLO};
+    interface IDictionary
+    {
+        public void AddWord(string word,List<string> words);
+        public void ChangeWord(string word);
+        public void RemoveWord(string word);
+        public void SearchTranslate(string word);
+        public void InputFile(string path);
+    }
+    class Dict : IDictionary
+    {
+        
+        public Dictionary<string,List<string>> dictionary = new Dictionary<string, List<string>>();
+        public TypeDictionary Type;
+        public Dict(TypeDictionary type)
+        {
+            this.Type = type;
+        }
+
+        //public void AddToFile(string path, Dictionary<string, List<string>> dict)
+        //{
+        //    using (FileStream fs = new FileStream(path,FileMode.OpenOrCreate))
+        //    {
+                
+        //    }
+        //}
+
+        public void AddWord(string word, List<string> words)
+        {
+            dictionary.Add(word, words);
+        }
+
+        public void ChangeWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InputFile(string path)
+        {
+            //File.WriteAllText(path, dictionary);
+        }
+
+        public void RemoveWord(string word,List<string>words)
+        {
+            dictionary.Remove(word);
+            //if (dictionary.ContainsKey(word))
+            //{
+            //    dictionary.Remove(word,out words);
+            //}
+        }
+
+        public void RemoveWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SearchTranslate(string word)
+        {
+            if (dictionary.ContainsKey(word))
+            {
+                foreach (var item in dictionary[word])
+                {
+                    Console.WriteLine($"Searched word - {item}");
+                }
+            }
+        }
+
+    }
+}
