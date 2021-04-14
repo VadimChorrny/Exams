1b0704a20c65dbf0ff07094b11326281da2c8b5c
VadimChorrny <chorrny228@gmail.com>
2021-04-14 11:40:18 +0300

    Add CreateQuiz

---
+++ Third_Task/QuestionData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Third_Task
+{
+    public class QuestionData
+    {
+        public int Number { get; set; }
+        public string Question { get; set; }
+        public string[] Answers { get; set; }
+        [JsonPropertyName("correct_answer")]
+        public int CorrectAnswer { get; set; }
+    }
+    public class Module
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int Questions { get; set; }
+        public DateTime Revision { get; set; }
+    }
+
+    public class ModuleData
+    {
+        public Module Module { get; set; }
+        public QuestionData[] Questions { get; set; }
+    }
+}
--- Third_Task/Victorina.cs
+++ Third_Task/Victorina.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 
 namespace Third_Task
 {
@@ -15,14 +18,10 @@ namespace Third_Task
     //    public void CreateVictorine();
     //}
 
-    interface IAdmin
-    {
-    }
-
     class Users
     {
         public string Login { get; set; }
-        public string Password 
+        public string Password
         {
             get => Login;
             set
@@ -31,7 +30,7 @@ namespace Third_Task
             }
         }
         public DateTime Date { get; set; } = new DateTime();
-        public Users(string log,string pass,DateTime time)
+        public Users(string log, string pass, DateTime time)
         {
             this.Login = log;
             this.Password = pass;
@@ -39,24 +38,11 @@ namespace Third_Task
         }
     }
 
-    class Questions
+    class Resourse
     {
-        // quest - file
-        // 
-        public Questions()
-        {
-
-        }
-    }
-
-    class Victorina
-    {
-        public List<Questions> questions = new List<Questions>();
-        public List<Users> users = new List<Users>();
-        public Users current;
-
-        public DateTime date = new DateTime();
+        // variable
         string login;
+        string password;
         public string Login
         {
             get => login;
@@ -68,7 +54,6 @@ namespace Third_Task
                     throw new Exception("Error! Login is Null!");
             }
         }
-        string password;
         public string Password
         {
             get => password;
@@ -81,27 +66,19 @@ namespace Third_Task
             }
         }
         public uint Score { get; set; }
-        
-        public List<string> mathQuestion = new List<string>()
-        {
-            "P = ", //0
-            "2 + 2 = ",//1
-            "1 + 3 = ",//2
-            "2 + 4 = ",//3
-            "4 + 5 = ",//4
-            "6 + 6 = ",//5
-            "5 + 7 = ",//6
-            "32 + 64 = ",//7
-            "21 + 23 = ", // 8
-            "45 + 54 = ",//9
-            "7 + 3 + 2 = "// 10
-        };
+
+        public List<QuestionData> quiz { get; set; } = new List<QuestionData>();
+        public List<Users> users = new List<Users>();
+        public DateTime date = new DateTime();
+        public Users current;
+
+        // Sign In
 
         public void SignIn(string pass, string login)
         {
             foreach (var item in users)
             {
-                if(item.Login == login && item.Password == pass)
+                if (item.Login == login && item.Password == pass)
                 {
                     current = item;
                     return;
@@ -114,143 +91,45 @@ namespace Third_Task
             users.Add(user);
         }
 
-        public void CreateVictorine(TypeQuestion type)
+        // Work with Victorina
+
+        public void CreateQuiz()
         {
-            if (type == TypeQuestion.Math)
+            //string json = File.ReadAllText("data.json");
+            //JsonSerializerOptions options = new JsonSerializerOptions();
+            //options.PropertyNameCaseInsensitive = true;
+            //var moduleData = System.Text.Json.JsonSerializer.Deserialize<ModuleData>(json, options);
+
+            foreach (var item in quiz)
             {
-                Console.WriteLine(mathQuestion[0]);
-                Console.Write("Enter answer ->");
-                string tmp0 = Console.ReadLine();
-                if (tmp0 == "3.14")
+                for (int i = 0; i < quiz.Count; i++)
                 {
-                    Console.WriteLine("Your answer - true");
-                    Score++;
-
-                    Console.WriteLine(mathQuestion[1]);
-                    Console.Write("Enter answer ->");
-                    int tmp1 = Convert.ToInt32(Console.ReadLine());
-                    if (tmp1 == 4)
+                    Console.WriteLine(item.Question[i]);
+                    Console.WriteLine(item.Answers);
+                    int tmp = Convert.ToInt32(Console.ReadLine());
+                    if (tmp == item.CorrectAnswer)
                     {
-                        Console.WriteLine("Your answer - true");
                         Score++;
-
-                        Console.WriteLine(mathQuestion[2]);
-                        Console.Write("Enter answer ->"); int tmp2 = Convert.ToInt32(Console.ReadLine());
-                        if (tmp2 == 4)
-                        {
-                            Console.WriteLine("Your answer - true");
-                            Score++;
-
-                            Console.WriteLine(mathQuestion[3]);
-                            Console.WriteLine("Enter answer ->"); int tmp3 = Convert.ToInt32(Console.ReadLine());
-                            if (tmp3 == 6)
-                            {
-                                Console.WriteLine("Your answer - true");
-                                Score++;
-
-                                Console.WriteLine(mathQuestion[4]);
-                                Console.WriteLine("Enter answer ->"); int tmp4 = Convert.ToInt32(Console.ReadLine());
-                                if (tmp4 == 9)
-                                {
-                                    Console.WriteLine("Your answer- true");
-                                    Score++;
-
-                                    Console.WriteLine(mathQuestion[5]);
-                                    Console.WriteLine("Enter answers ->"); int tmp5 = Convert.ToInt32(Console.ReadLine());
-                                    if (tmp5 == 12)
-                                    {
-                                        Console.WriteLine("Your answer - true");
-                                        Score++;
-
-                                        Console.WriteLine(mathQuestion[6]);
-                                        Console.WriteLine("Enter answers ->"); int tmp6 = Convert.ToInt32(Console.ReadLine());
-                                        if (tmp6 == 12)
-                                        {
-                                            Console.WriteLine("Your answer - true");
-                                            Score++;
-
-                                            Console.WriteLine(mathQuestion[7]);
-                                            Console.WriteLine("Enter answers ->"); int tmp7 = Convert.ToInt32(Console.ReadLine());
-                                            if (tmp7 == 96)
-                                            {
-                                                Console.WriteLine("Your answer - true");
-                                                Score++;
-
-                                                Console.WriteLine(mathQuestion[8]);
-                                                Console.WriteLine("Enter answers ->"); int tmp8 = Convert.ToInt32(Console.ReadLine());
-                                                if (tmp8 == 44)
-                                                {
-                                                    Console.WriteLine("Your answer - true");
-                                                    Score++;
-
-                                                    Console.WriteLine(mathQuestion[9]);
-                                                    Console.WriteLine("Enter answers ->"); int tmp9 = Convert.ToInt32(Console.ReadLine());
-                                                    if (tmp9 == 99)
-                                                    {
-                                                        Console.WriteLine("Your answer - true");
-                                                        Score++;
-                                                    }
-                                                    else
-                                                    {
-                                                        Console.WriteLine("error");
-                                                        return;
-                                                    }
-                                                }
-                                                else
-                                                {
-                                                    Console.WriteLine("error");
-                                                    return;
-                                                }
-                                            }
-                                            else
-                                            {
-                                                Console.WriteLine("error");
-                                                return;
-                                            }
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("error");
-                                            return;
-                                        }
-
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("error");
-                                        return;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("error!");
-                                return;
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Your answer - false");
-                            return;
-                        }
-
                     }
                     else
                     {
-                        Console.WriteLine("Your answer - false");
+                        Console.WriteLine("Hueta answer!");
                         return;
                     }
-
-
-                }
-                // maybe add here !!!!!!!
-                else
-                {
-                    Console.WriteLine("Error answer!");
                 }
             }
-            Console.WriteLine($"Your count score - {Score}");
+
         }
+
+
+
+        public void TopUsers()
+        {
+            Console.WriteLine("______Top Users");
+            
+        }
+
+        // Menu
 
         public void ShowMenu()
         {
@@ -262,17 +141,14 @@ namespace Third_Task
                 action = Convert.ToInt32(Console.ReadLine());
                 if (action == 1)
                 {
-                    CreateVictorine(TypeQuestion.Math);
+                    //CreateVictorine(TypeQuestion.Math);
                 }
 
             } while (true);
 
         }
 
-        public void CreateVictorine()
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
 
